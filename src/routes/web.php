58ac2290ef460702f4ec00154534f2ff48b654e5
FumiyaAdachi <adachi@launchcraft.jp>
2021-11-30 21:15:52 +0900

    ルーティングを修正しました

--- src/routes/web.php
+++ src/routes/web.php
@@ -13,13 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+  return view('welcome');
+});
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/','TodosController@index');
 Route::resource('todos','TodosController');
